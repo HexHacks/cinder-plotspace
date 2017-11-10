adcfb3ab39ff8bcf72ace503673176fb4ea58b71
Jacob Peyron <jacob.peyron@gmail.com>
2017-11-10 16:38:34 +0100

    Initial state for space plotter

---
+++ include/LineSpace.h
@@ -0,0 +1,157 @@
+//
+//  LineSpace.h
+//  PlotSpace
+//
+//  Created by Jacob Peyron on 10/11/17.
+//
+//
+
+#include "cinder/CinderGlm.h"
+#include "cinder/gl/gl.h"
+#include "cinder/gl/VboMesh.h"
+#include "cinder/GeomIo.h"
+#include <vector>
+
+#ifndef LineSpace_h
+#define LineSpace_h
+
+class LineSpace
+{
+    using vec3 = ci::vec3;
+    using vec4 = ci::vec4;
+    using ivec3 = ci::ivec3;
+    using VboMesh = ci::gl::VboMesh;
+    using VboMeshRef = ci::gl::VboMeshRef;
+    
+    vec3 mSize;
+    vec3 mHSize;
+    ivec3 mCount;
+    
+    VboMeshRef mVbo;
+    
+public:
+    LineSpace(const vec3& size = vec3(10.), const ivec3& count = ivec3(10)) :
+        mSize(size),
+        mHSize(size * 0.5f),
+        mCount(count)
+    {}
+    
+    VboMeshRef getVbo() { return mVbo; }
+    vec3 getSize() const { return mSize; }
+    ivec3 getCount() const { return mCount; }
+    
+    ivec3 getIdxFromFlat(size_t idx) const
+    {
+        return ivec3(idx%mCount.x, idx/(mCount.x*mCount.z),(idx/mCount.x)%mCount.z);
+    }
+    
+    size_t getFlatFromIdx(const ivec3& idx) const
+    {
+        return idx.x + mCount.x * (idx.y + mCount.z * idx.z);
+    }
+    
+    std::vector<GLuint> getNeighbours(const ivec3& idx) const
+    {
+        static std::vector<ivec3> diffs{
+            ivec3(-1, 0, 0),
+            ivec3(1, 0, 0),
+            ivec3(0, -1, 0),
+            ivec3(0, 1, 0),
+            ivec3(0, 0, -1),
+            ivec3(0, 0, 1)
+        };
+        
+        std::vector<GLuint> out;
+        for (const auto& d : diffs)
+        {
+            auto n = idx + d;
+            if (n.x >= 0 && n.x < mCount.x &&
+                n.y >= 0 && n.y < mCount.y &&
+                n.z >= 0 && n.z < mCount.z)
+                out.push_back(getFlatFromIdx(n));
+        }
+        return out;
+    }
+    
+    std::vector<GLuint> createIndices() const
+    {
+        std::vector<GLuint> out;
+        for (size_t y = 0; y < mCount.y; y++)
+        {
+            for (size_t z = 0; z < mCount.z; z++)
+            {
+                for (size_t x = 0; x < mCount.x; x++)
+                {
+                    ivec3 currIdx(x, y, z);
+                    auto flat = getFlatFromIdx(currIdx);
+                    auto nei = getNeighbours(currIdx);
+                    
+                    for (const auto& n : nei)
+                    {
+                        out.push_back(flat);
+                        out.push_back(n);
+                    }
+                }
+            }
+        }
+        
+        return out;
+    }
+    
+    vec3 standardFunc(const ivec3& idx) const
+    {
+        vec3 zp(idx.x/float(mCount.x), idx.y/float(mCount.y), idx.z/float(mCount.z));
+        return zp * mSize - mHSize;
+    }
+    
+    std::vector<vec3> getStandardPositions() const
+    {
+        std::vector<vec3> out(mCount.x*mCount.y*mCount.z);
+        
+        for (size_t i = 0; i < out.size(); i++)
+        {
+            auto idx = getIdxFromFlat(i);
+            out[i] = standardFunc(idx);
+        }
+        
+        return out;
+    }
+    
+    std::vector<vec4> getStandardColors() const
+    {
+        return std::vector<vec4>(mCount.x*mCount.y*mCount.z, vec4(1.));
+    }
+    
+    VboMeshRef generateVbo()
+    {
+        const auto cPosAttrib = ci::geom::Attrib::POSITION;
+        const auto cColAttrib = ci::geom::Attrib::COLOR;
+        std::vector<VboMesh::Layout> bufferLayout = {
+            VboMesh::Layout().usage( GL_DYNAMIC_DRAW ).attrib( cPosAttrib, 3 ),
+            VboMesh::Layout().usage( GL_DYNAMIC_DRAW ).attrib( cColAttrib, 4 )
+        };
+        
+        size_t vertCount = mCount.x*mCount.y*mCount.z;
+        
+        
+        auto indices = createIndices();
+        mVbo = VboMesh::create(vertCount, GL_LINES, bufferLayout, indices.size(), GL_UNSIGNED_INT);
+        
+        mVbo->bufferIndices(indices.size() * sizeof(GLuint), indices.data());
+        
+        auto pos = getStandardPositions();
+        auto col = getStandardColors();
+        mVbo->bufferAttrib(cPosAttrib, pos.size() * sizeof(vec3), pos.data());
+        mVbo->bufferAttrib(cColAttrib, col.size() * sizeof(vec4), col.data());
+        
+        return mVbo;
+    }
+    
+    void draw()
+    {
+        if (mVbo)
+            ci::gl::draw(mVbo);
+    }
+};
+
+#endif /* LineSpace_h */
--- src/PlotSpaceApp.cpp
+++ src/PlotSpaceApp.cpp
@@ -1,34 +1,181 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "cinder/CameraUi.h"
+#include "cinder/params/Params.h"
+
+#include "LineSpace.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+static float clamp(float v, float min, float max)
+{
+    if (v < min)
+        return min;
+    else if (v > max)
+        return max;
+    else
+        return v;
+}
+
 class PlotSpaceApp : public App {
+    CameraPersp mCam;
+    CameraUi mCamUi;
+    params::InterfaceGlRef mParams;
+    
+    LineSpace mSpace;
+    
+    float mT;
+    bool mAnimate;
+    
   public:
 	void setup() override;
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
+    
+    void calcSpace();
+    vec3 func(const ivec3& idx, const vec3& pos);
+    vec4 col(const ivec3& idx, const vec3& pos);
+    
+    void prepareSettings( Settings *settings )
+    {
+        settings->setHighDensityDisplayEnabled();
+    }
+    
+    void resize() override
+    {
+        mCam.setAspectRatio(getWindowAspectRatio());
+    }
+    
+    void setupSpace()
+    {
+        mSpace.generateVbo();
+    }
+    
+    void setupParams()
+    {
+        mParams = params::InterfaceGl::create( getWindow(), "Parameters", ivec2( 200, 200 ) );
+        mParams->addParam("T", &mT)
+            .min(0.0)
+            .max(10.0)
+            .step(0.1)
+            .keyIncr("+")
+            .keyDecr("-")
+            .updateFn([this](){ calcSpace(); });
+
+        mParams->addParam("Animate", &mAnimate).key("a");
+    }
+    
 };
 
 void PlotSpaceApp::setup()
 {
+    setWindowSize(500, 500);
+    mT = 0.;
+    mAnimate = false;
+    
+    mCam.lookAt(vec3(0.f, 3.f, 3.f), vec3(0.f));
+    mCamUi = CameraUi(&mCam, getWindow());
+    
+    setupSpace();
+    setupParams();
+    
+    gl::enableDepthRead();
+    gl::enableDepthWrite();
 }
 
 void PlotSpaceApp::mouseDown( MouseEvent event )
 {
 }
 
+static vec4 v4(const vec3& v)
+{
+    return vec4(v.x, v.y, v.z, 1.);
+}
+
+static vec3 v3(const vec4& v)
+{
+    return vec3(v.x / v.w, v.y / v.w, v.z / v.w);
+}
+
+static mat4 m4(float aa, float ab, float ac, float ad,
+               float ba, float bb, float bc, float bd,
+               float ca, float cb, float cc, float cd,
+               float da, float db, float dc, float dd)
+{
+    return mat4(aa, ba, ca, da,
+                ab, bb, cb, db,
+                ac, bc, cc, dc,
+                ad, bd, cd, dd);
+}
+
+vec3 PlotSpaceApp::func(const ivec3& idx, const vec3& pos)
+{
+    auto T = m4(1., 0., 0., 0.,
+                0., 1., 0., 0.,
+                0., 0., 1., 0.,
+                0.1*sin(pos.x+mT), 0.1*cos(pos.y*pos.z+mT), 0., 1.);
+
+    
+    return v3(T * v4(pos));
+}
+
+vec4 PlotSpaceApp::col(const ivec3& idx, const vec3& pos)
+{
+    vec4 col(1.);
+    float outer = length(mSpace.getSize()) * 0.5f;
+    float rad = length(pos) / outer;
+    
+    col.r = 1.f-rad;
+    col.g = clamp(1.f/(rad*rad+1.f), 0.f, 1.f);
+    col.w = rad;
+    
+    return col;
+}
+
+void PlotSpaceApp::calcSpace()
+{
+    auto vbo = mSpace.getVbo();
+    auto posIter = vbo->mapAttrib3f(geom::Attrib::POSITION);
+    auto colIter = vbo->mapAttrib4f(geom::Attrib::COLOR);
+    for (size_t i = 0; i < vbo->getNumVertices(); i++)
+    {
+        auto idx = mSpace.getIdxFromFlat(i);
+        auto pos = mSpace.standardFunc(idx);
+        
+        *posIter = func(idx, pos);
+        *colIter = col(idx, pos);
+        
+        posIter++;
+        colIter++;
+    }
+    
+    colIter.unmap();
+    posIter.unmap();
+}
+
 void PlotSpaceApp::update()
 {
+    if (mAnimate)
+    {
+        mT = 5.f*(sin(getElapsedSeconds())+1.f);
+        calcSpace();
+    }
 }
 
 void PlotSpaceApp::draw()
 {
-	gl::clear( Color( 0, 0, 0 ) ); 
+    gl::clear( Color::gray(0.3) );
+    gl::setMatrices(mCam);
+    
+    
+    mSpace.draw();
+    gl::drawCoordinateFrame(1.);
+    
+    mParams->draw();
 }
 
 CINDER_APP( PlotSpaceApp, RendererGl )
